0f3156f39d5662f7d3788b5fd072a7b2961c2052
AlexMartGong <155708055+AlexMartGong@users.noreply.github.com>
2024-10-10 21:55:39 -0600

    Navigation is working

--- app/src/main/java/com/example/damagereport/Navigation.kt
+++ app/src/main/java/com/example/damagereport/Navigation.kt
@@ -1,24 +1,45 @@
 package com.example.damagereport
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Navigation : AppCompatActivity() {
+
+    private lateinit var bnv: BottomNavigationView
+    private lateinit var frame: FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_navigation)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        bnv = findViewById(R.id.bottomNavigationView)
+        frame = findViewById(R.id.fragmentContainer)
+
+        // Configurar el listener para el BottomNavigationView
+        bnv.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
         }
 
-
+        // Cargar el Fragment inicial
+        loadFragment(HomeFragment())
     }
 
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
 }
